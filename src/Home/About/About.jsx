856b3f8d07f2a7c8048ee1328799fe2ad788ef32
Abu Abdullah <abdullah72308@gmail.com>
2023-06-03 18:34:58 +0600

    banner added

--- src/Home/About/About.jsx
+++ src/Home/About/About.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            
+        <div className='flex flex-col items-center justify-center'>
+            <p className='md:text-4xl font-bold mb-3 mt-10 md:mt-16 text-center uppercase'>About Me</p>
+            <div className="divider md:w-1/3 mx-auto my-3"><img className='w-16' src="https://www.logomaker.com/api/main/images/1j+ojFVDOMkX9Wytexe43D6kh...KFrBFJmh3FwXs1M3EMoAJtligogftp8PU8" alt="" /></div>
+            <p className='md:font-bold md:w-1/2 text-center md:text-xl'>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+            <div>
+                
+            </div>
         </div>
     );
 };
